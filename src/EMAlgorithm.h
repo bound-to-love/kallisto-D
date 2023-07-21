ea4a4454d160c8586f4292da2618b59d858c3643
Rebekah Loving <bekahloving@gmail.com>
2023-07-20 22:57:56 -0700

    Update EMAlgorithm.h

--- src/EMAlgorithm.h
+++ src/EMAlgorithm.h
@@ -146,7 +146,7 @@ struct EMAlgorithm {
         std::cerr << "countNorm: " << countNorm << std::endl; std::cerr.flush();
         //std::cerr <<"numEC is " << numEC << std::endl; std::cerr.flush(); 
         for (auto t_it = 0; t_it < numEC; ++t_it) {
-          std::cerr <<"t_it is: " << t_it << ", alpha is: " << alpha_[trs[t_it]] << " wv[t_it] is: " << wv[t_it] << std::endl; std::cerr.flush(); 
+          std::cerr <<"t_it is: " << t_it << ", alpha is: " << alpha_[trs[t_it]] << " wv[t_it] is: " << wv[t_it] << " trs[t_it] is:" << trs[t_it] << std::endl; std::cerr.flush(); 
           next_alpha[trs[t_it]] += (wv[t_it] * alpha_[trs[t_it]]) * countNorm;
         }
 
