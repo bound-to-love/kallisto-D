9184dcae82f0dfac79b35151879ced199c718785
Rebekah Loving <bekahloving@gmail.com>
2023-08-10 15:39:45 -0700

    Update EMAlgorithm.h

--- src/EMAlgorithm.h
+++ src/EMAlgorithm.h
@@ -216,7 +216,7 @@ struct EMAlgorithm {
     //TRYING PLACING HERE INSTEAD 
     for (const auto& it : ecmapinv_) {
       if (it.first.cardinality() == 1) {
-        next_alpha[it.first.maximum()] += counts_[it.second];
+        alpha[it.first.maximum()] += counts_[it.second];
         //std::cerr << "cardinality 1 : " << it.first.maximum() << " it.second: " << it.second << " counts_[it.second]: " << counts_[it.second] << std::endl; std::cerr.flush(); 
       }
     }
