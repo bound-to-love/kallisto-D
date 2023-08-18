08712796655868224aa48e2cd7ad9b6007f2e32e
Rebekah Loving <bekahloving@gmail.com>
2023-08-17 17:27:04 -0700

    Update KmerIndex.cpp

--- src/KmerIndex.cpp
+++ src/KmerIndex.cpp
@@ -1383,7 +1383,7 @@ void KmerIndex::match(const char *s, int l, std::vector<std::pair<const_UnitigMa
   }
 
   size_t proc = 0;
-  while (proc < l - k) {
+  while (proc < l - k + 1) {
     const_UnitigMap<Node> um = dbg.findUnitig(s, proc, l);
     if (um.isEmpty) {
       proc++;
